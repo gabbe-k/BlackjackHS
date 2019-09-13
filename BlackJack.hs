e6c1cf050029d6689fd2c02daf8d2d108bba5afc
gabbe-k <gabbekall@gmail.com>
2019-09-13 12:06:28 +0200

    Task a2 done! Alternatives linked

--- BlackJack.hs
+++ BlackJack.hs
@@ -7,7 +7,7 @@ import Test.QuickCheck hiding (shuffle)
 
 --Task A1
 
-hand2 = [Card (Numeric 2) Hearts, Card Jack Spades]
+hand2 = [Card (Numeric 2) Hearts, Card Jack Spades, Card King Diamonds, Card Queen Spades]
 
 sizeSteps :: [Int]
 sizeSteps = [ size hand2
@@ -32,14 +32,37 @@ aHand = [aCard1, aCard2]
 
 displayCard :: Card -> String
 displayCard (Card (Numeric r) s) = show (r) ++ " of " ++ show(s) 
-displayCard (Card r s) = show (r) ++ " of " ++ show(s) 
+displayCard (Card r s)           = show (r) ++ " of " ++ show(s) 
 
 display :: Hand -> String
---display hand = unlines [displayCard card | card <- hand]
-display card = displayCard(card)
-display (card:hand) = displayCard(card) (++) display(hand)
+display hand = "Your hand: \n" ++  unlines [ " >  " ++displayCard card | card <- hand] ++ "\n"
 
-main = putStrLn (displayCard aCard1)  
+{-display :: Hand -> String
+display []           = []
+display (card:hand) | length hand == 0 = (displayCard(card)) ++  display(hand) 
+                    | otherwise = (displayCard(card)) ++ ", " ++  display(hand) 
+-}
+{-
+display :: Hand -> String
+display [] _          = []
+display (card:hand) (handLen) | length length  == (length (hand) + 1) = "Your current hand: "
+                                  | length hand == 0 = (displayCard(card)) ++  display(hand) 
+                                  | otherwise = "Len cardhand " ++ show(length (card:hand)) ++ " " ++ "Len hand " ++ show(length(hand)) ++ " ||| " ++ (displayCard(card)) ++ ", " ++  display(hand) 
+    where handLen = length(hand)
+-}
+
+main = putStr (display hand2)  
+
+--Task A3
+valueRank :: Rank -> Int
+valueRank (Card (Numeric n))
+valueRank (Card r)
+
+valueCard :: Card -> Int
+
+numberOfAces :: Hand -> Int
+
+value :: Hand -> Int
 
 --display :: Hand -> String
 --display aHand =  
