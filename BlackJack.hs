153eb29c04ae6434e8560d3ff6139e345ca11910
gabbe-k <gabbekall@gmail.com>
2019-09-13 14:16:18 +0200

    A3 and A4 done
    Need to comment code

--- BlackJack.hs
+++ BlackJack.hs
@@ -5,18 +5,6 @@ import RunGame
 import System.IO
 import Test.QuickCheck hiding (shuffle)
 
---Task A1
-
-hand2 = [Card (Numeric 2) Hearts, Card Jack Spades, Card King Diamonds, Card Queen Spades]
-
-sizeSteps :: [Int]
-sizeSteps = [ size hand2
-            , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
-            , 1 + size (Card Jack Spades : [])
-            , 2 + size []
-            , 2
-            ]
-
 --Useful definitions
 
 aCard1 :: Card 
@@ -25,8 +13,23 @@ aCard1 = Card (Numeric 6) Diamonds
 aCard2 :: Card
 aCard2 = Card King Spades
 
+aCard3 :: Card
+aCard3 = Card Ace Spades
+
 aHand :: Hand 
 aHand = [aCard1, aCard2]
+
+hand2 = [Card (Numeric 2) Hearts, Card Jack Spades, Card Ace Diamonds, Card Ace Spades]
+
+--Task A1
+
+sizeSteps :: [Int]
+sizeSteps = [ size hand2
+            , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
+            , 1 + size (Card Jack Spades : [])
+            , 2 + size []
+            , 2
+            ]
 
 --Task A2
 
@@ -37,32 +40,39 @@ displayCard (Card r s)           = show (r) ++ " of " ++ show(s)
 display :: Hand -> String
 display hand = "Your hand: \n" ++  unlines [ " >  " ++displayCard card | card <- hand] ++ "\n"
 
-{-display :: Hand -> String
-display []           = []
-display (card:hand) | length hand == 0 = (displayCard(card)) ++  display(hand) 
-                    | otherwise = (displayCard(card)) ++ ", " ++  display(hand) 
--}
-{-
-display :: Hand -> String
-display [] _          = []
-display (card:hand) (handLen) | length length  == (length (hand) + 1) = "Your current hand: "
-                                  | length hand == 0 = (displayCard(card)) ++  display(hand) 
-                                  | otherwise = "Len cardhand " ++ show(length (card:hand)) ++ " " ++ "Len hand " ++ show(length(hand)) ++ " ||| " ++ (displayCard(card)) ++ ", " ++  display(hand) 
-    where handLen = length(hand)
--}
-
 main = putStr (display hand2)  
 
 --Task A3
 valueRank :: Rank -> Int
-valueRank (Card (Numeric n))
-valueRank (Card r)
+valueRank (Numeric r) = r
+valueRank r | r == Ace = 11
+            | otherwise = 10
 
 valueCard :: Card -> Int
+valueCard (Card r _) = valueRank r
 
 numberOfAces :: Hand -> Int
+numberOfAces [] = 0
+numberOfAces (card:hand) | rank card == Ace = 1 + numberOfAces(hand)
+                         | otherwise = numberOfAces(hand)
 
 value :: Hand -> Int
+value [] = 0
+value (card:hand) | valueCard(card) + value(hand) < 21 = valueCard(card) + value(hand)
+                  | otherwise = valueCard(card) + value(hand) - (10 * numberOfAces(card:hand)) 
 
---display :: Hand -> String
---display aHand =  
+--Task A4
+
+gameOver :: Hand -> Bool
+gameOver hand | value(hand) > 21 = True
+              | otherwise = False
+
+highestValHand :: Hand -> Hand -> Player
+highestValHand guestHand bankHand | value(guestHand) > value(bankHand) = Guest
+                                  | otherwise = Bank
+
+
+winner :: Hand -> Hand -> Player 
+winner guestHand bankHand | value(guestHand) == value(bankHand) = Bank
+                          | (value(guestHand) > 21) && (value(bankHand) > 21)  = Bank
+                          | (value(guestHand)<= 21)  || (value(bankHand) <= 21) = highestValHand guestHand bankHand
