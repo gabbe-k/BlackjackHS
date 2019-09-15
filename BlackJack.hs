e43d74850a3729bf68a37b546bea7b4b11f688f0
gabbe-k <gabbekall@gmail.com>
2019-09-15 20:50:19 +0200

    Update BlackJack.hs

--- BlackJack.hs
+++ BlackJack.hs
@@ -18,16 +18,17 @@ aCard3 = Card Ace Spades
 aHand :: Hand 
 aHand = [aCard1, aCard2]
 
-hand2 = [Card (Numeric 2) Hearts, Card Jack Spades, Card Ace Diamonds, Card Ace Spades]
+aHand2 = [Card (Numeric 7) Hearts, Card Ace Spades, Card Ace Diamonds]
 
 --Task A1
 --Example showing how a recursive function would process a list
 
 sizeSteps :: [Int]
-sizeSteps = [ size hand2
+sizeSteps = [ size aHand2
             , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
             , 1 + size (Card Jack Spades : [])
-            , 2 + size []
+            , 1 + (1 + size [])
+            , 1 + (1 + 0)
             , 2
             ]
 
@@ -43,21 +44,18 @@ displayCard (Card r s)           = show (r) ++ " of " ++ show(s)
 display :: Hand -> String
 display hand = "Your hand: \n" ++  unlines [ " >  " ++displayCard card | card <- hand]
 
---putStrLn is necessary to aquire the right formatting (converts \n to newlines)
-main = putStrLn (display hand2)  
+--Example: putStr is necessary to aquire the right formatting (converts \n to newlines)
+displayExamp = putStr (display aHand2)  
 
 --Task A3
 --Functions for counting the points given to the player for having cards/hands
 
 --Returns points associated with certain ranks
 valueRank :: Rank -> Int
-valueRank (Numeric r) = r
-valueRank r | r == Ace = 11
-            | otherwise = 10
-
---Helper function for the previous function
-valueCard :: Card -> Int
-valueCard (Card r _) = valueRank r
+valueRank (Numeric r)   = r
+--Aces are worth one if sum of hand exceeds 21
+valueRank Ace           = 11 
+valueRank r             = 10
 
 --Counts the number of aces in a hand
 numberOfAces :: Hand -> Int
@@ -68,8 +66,8 @@ numberOfAces (card:hand) | rank card == Ace = 1 + numberOfAces(hand)
 --Counts the total value of a players hand, and deletes points if value > 21
 value :: Hand -> Int
 value [] = 0
-value (card:hand) | valueCard(card) + value(hand) <= 21 = valueCard(card) + value(hand)
-                  | otherwise = valueCard(card) + value(hand) - (10 * numberOfAces(card:hand)) 
+value (card:hand) | valueRank(rank(card)) + value(hand) <= 21 = valueRank(rank(card)) + value(hand)
+                  | otherwise = valueRank(rank(card)) + value(hand) - (10 * numberOfAces(card:hand)) 
 
 --Task A4
 --Functions used for checking various win/lose conditions
