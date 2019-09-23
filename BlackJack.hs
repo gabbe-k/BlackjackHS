2d9cee84d7ac299b4e9f8ab6b758d47a1be48d30
gabbe-k <44163424+gabbe-k@users.noreply.github.com>
2019-09-23 16:44:07 +0200

    final commit

--- BlackJack.hs
+++ BlackJack.hs
@@ -1,37 +1,24 @@
-{- Blackjack (Lab2)
+﻿{- Blackjack (Lab2)
    Authors: Daniel Kvist, Daniel Perlkvist, Gabriel Käll
    Lab group: 69
  -}
 
 module Blackjack where
 
-
 import Cards
 import RunGame
 import Test.QuickCheck hiding (shuffle)
-import Data.List
 
---Useful definitions
+-- A1 ------------------------
 
-aCard1 :: Card 
-aCard1 = Card (Numeric 6) Diamonds
+-- An example Hand containing 2 cards
+hand2 :: Hand
+hand2 = [Card (Numeric 2) Hearts, Card Jack Spades]
 
-aCard2 :: Card
-aCard2 = Card (Numeric 6) Spades
-
-aCard3 :: Card
-aCard3 = Card Ace Spades
-
-aHand :: Hand 
-aHand = [aCard1, aCard2]
-
-aHand2 = [Card (Numeric 8) Hearts, Card Ace Diamonds]
-
---Task A1
---Example showing how a recursive function would process a list
-
+-- Produces a list containing several identical elements
+-- of the type Int, provided (length hand2) == 2
 sizeSteps :: [Int]
-sizeSteps = [ size aHand2
+sizeSteps = [ size hand2
             , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
             , 1 + size (Card Jack Spades : [])
             , 1 + (1 + size [])
@@ -39,72 +26,78 @@ sizeSteps = [ size aHand2
             , 2
             ]
 
---Task A2
---Functions used for displaying cards and hands neatly for the user
 
---Displays a card in the format "x of y"
+-- A2 ------------------------
+
+
+-- Two predetermined cards used for aHand
+aCard1 :: Card
+aCard1 = Card Ace Spades
+
+aCard2 :: Card
+aCard2 = Card (Numeric 7) Clubs
+
+-- A hand containing two predetermined cards
+aHand :: Hand
+aHand = [aCard1, aCard2]
+
+
+-- Displays a given Card as a string reading
+-- which card it is in common tongue
 displayCard :: Card -> String
-displayCard (Card (Numeric r) s) = show (r) ++ " of " ++ show(s) 
-displayCard (Card r s)           = show (r) ++ " of " ++ show(s) 
+displayCard (Card (Numeric r) s)    = (show r) ++ " of " 
+                                   ++ (show s)
+displayCard (Card r s)              = "The " ++ (show r) 
+                                   ++ " of " ++ (show s)
 
---Displays all cards in a hand using previous function and list comprehension
+
+-- Displays a given hand as a string telling you
+-- how many, and which cards are in the hand
 display :: Hand -> String
-display hand = "Your hand: \n" ++ unlines [ " >  " ++displayCard card | card <- hand]
+display hand            =  "You have " ++ (show(size hand))
+                        ++ " cards on hand:\n" 
+                        ++ unlines [displayCard card | card <- hand]
 
---Example: putStr is necessary to aquire the right newline formatting
-displayExamp = putStr (display aHand2)  
 
---Task A3
---Functions counting the points given to the player for having cards/hands
+-- A3 ------------------------
 
---Returns points associated with certain ranks
+-- Assigns all card ranks a corresponding numeric value
 valueRank :: Rank -> Int
-valueRank (Numeric r)   = r
---Aces are worth one if sum of hand exceeds 21
-valueRank Ace           = 11 
-valueRank r             = 10
+valueRank (Numeric n)    = n
+valueRank Ace            = 11
+valueRank n              = 10
 
---Counts the number of aces in a hand
+-- Counts how many aces are in a given hand
 numberOfAces :: Hand -> Int
-numberOfAces [] = 0
-numberOfAces (card:hand) | rank card == Ace = 1 + numberOfAces(hand)
-                         | otherwise = numberOfAces(hand)
+numberOfAces []                   = 0
+numberOfAces ((Card Ace _):hand)  = 1 + (numberOfAces hand)
+numberOfAces (card:hand)          = numberOfAces hand
 
---Counts the total value of a players hand
---Deletes 10 points for each ace if value > 21
 
-value :: Hand -> Int
-value [] = 0
-value (card:hand) | valueRank(rank(card)) + value(hand) > 21 
-                   && numberOfAces(card:hand) > 0
-                   = valueRank(rank(card)) + value(hand) 
-                   - (10 * numberOfAces(card:hand)) 
-                  | otherwise 
-                   = valueRank(rank(card)) + value(hand) 
-
--- Calculates the total value of a given hand
-
-{-
 -- Fetches the value of a card depending on its rank
 valueCard :: Card -> Int
 valueCard (Card r _)     = valueRank r
 
+-- Calculates the total value of a given hand
 value :: Hand -> Int
 value []   = 0
-value hand | ((sum [valueRank(rank(card)) card | card <- hand]) > 21)
+value hand | ((sum [valueCard card | card <- hand]) > 21)
              && ((numberOfAces hand) > 0) 
-             = (sum [valueRank(rank(card)) card | card <- hand]) - 10*(numberOfAces hand)
-           | otherwise = sum [valueRank(rank(card)) card | card <- hand]    -}               
- 
-    
---Task A4
---Functions used for checking various win/lose conditions
+             = (sum [valueCard card | card <- hand]) - 10*(numberOfAces hand)
+           | otherwise = sum [valueCard card | card <- hand]
 
---Checks if a player is bust
+
+
+-- A4 ------------------------
+
+
+-- Determines if the value of a hand exceeds 21
 gameOver :: Hand -> Bool
-gameOver hand = value(hand) > 21 
+gameOver hand = (value hand) > 21
 
---Checks which player has won depending on boths hands
+
+-- Determines the winner of the round depending on the value of the hands,
+-- with both hands being equal in value results in the bank winning
 winner :: Hand -> Hand -> Player 
 winner handG handB | not (gameOver(handG)) &&  
                      (value(handG) > value(handB)) = Guest
@@ -112,13 +105,13 @@ winner handG handB | not (gameOver(handG)) &&
                      && (gameOver(handB))          = Guest
                    | otherwise                     = Bank
 
---B1
+-- B1 ------------------------
 
---A list of all ranks
+-- A list of all ranks
 allRanks :: [Rank]
 allRanks  = [Numeric n | n <- [2..10]] ++ [Jack,Queen,King,Ace]
 
---A list of all suits
+-- A list of all suits
 allSuits :: [Suit]
 allSuits = [Hearts, Spades, Diamonds, Clubs]
 
@@ -128,65 +121,64 @@ fullDeck = [Card rank suit | rank <- allRanks , suit <- allSuits]
 
 prop_size_fullDeck = size fullDeck == 52
 
---B2
+-- B2 ------------------------
 
 -- Draws a card from the deck and adds to the hand of whomever drew
 draw :: Deck -> Hand -> (Deck, Hand)
 draw deck hand | deck == [] = error "draw: The deck is empty."
                | otherwise  = ((drop 1 deck), (hand ++ take 1 deck))
 
---B3
+-- B3 ------------------------
 
---Plays for the bank
+
+-- Plays for the bank
 playBank :: Deck -> Hand
 playBank deck = playBank' (deck, [])
 
---Does the work for playbank
---Draws cards until the value of bankhand exceeds 16
+-- Does the work for playbank
+-- Draws cards until the value of bankhand exceeds 16
 playBank' :: (Deck, Hand) -> Hand
-playBank' deckBankH | value(snd(drawHand)) < 17 = playBank'(drawHand)
-                    | otherwise = h
-    where d = fst(deckBankH) 
-          h = snd(deckBankH)
-          drawHand = draw d h
+playBank' (d,h) | value (h) < 17 = playBank'(drawHand)
+                | otherwise = h
+    where 
+        drawHand = draw d h
 
---B4 
+-- B4 ------------------------ 
 
---Deletes the nth element in a deck
+-- Deletes the nth element in a deck
 deleteN :: Int -> Deck -> Deck
 deleteN i deck = xa ++ tail xb
     where 
-    xa = fst(splitAt (i) deck) --Beginning of deck 
-    xb = snd(splitAt (i) deck) --element i + Rest of deck 
+    (xa,xb) = splitAt (i) deck
 
---Shuffles a deck
+-- Shuffles a deck
 shuffle :: [Double] -> Deck -> Deck
-shuffle randList deck | deck == []     = deck
-                      | otherwise      = (card) : 
-                        shuffle (tail randList) (deleteN i deck)
+shuffle (x:xs) deck | deck == []     = deck
+                    | otherwise      = (card) 
+                    : shuffle xs (deleteN i deck)
     where 
-    i = ceiling ((size deck) * (head randList)) - 1 -- Chosen index
+    i = ceiling ((size deck) * x) - 1 -- Chosen index
     card = deck !! i --The randomly chosen card
 
---Task B5 
---Properties that the func shuffle must follow
+-- B5 ------------------------ 
+-- Properties that the func shuffle must follow
 
---Helper function for prop_shuffle
+-- Helper function for prop_shuffle
 belongsTo :: Card -> Deck -> Bool
 c `belongsTo` []      = False
 c `belongsTo` (c':cs) = c == c' || c `belongsTo` cs
 
---Checks if there is exactly one of each card in the shuffled deck
+-- Checks if there is exactly one of each card in the shuffled deck
 prop_shuffle :: Card -> Deck -> Rand -> Bool
 prop_shuffle card deck (Rand randomlist) =
     card `belongsTo` deck == card `belongsTo` shuffle randomlist deck
 
---Checks if the length of the deck is the same as before shuffling
+-- Checks if the length of the deck is the same as before shuffling
 prop_size_shuffle :: Rand -> Deck -> Bool
 prop_size_shuffle (Rand randomlist) deck = 
                    length(deck) == length(shuffle randomlist deck)
 
---B6
+-- B6 ------------------------
 
 implementation = Interface
   {  iFullDeck  = fullDeck
@@ -201,4 +193,3 @@ implementation = Interface
 
 main :: IO ()
 main = runGame implementation
-
--- BlackJack_Own.hs
+++ BlackJack_Own.hs
@@ -1,24 +1,37 @@
-﻿{- Blackjack (Lab2)
+{- Blackjack (Lab2)
    Authors: Daniel Kvist, Daniel Perlkvist, Gabriel Käll
    Lab group: 69
  -}
 
 module Blackjack where
 
+
 import Cards
 import RunGame
+import Test.QuickCheck hiding (shuffle)
+import Data.List
 
+--Useful definitions
 
--- A1 ------------------------
+aCard1 :: Card 
+aCard1 = Card (Numeric 6) Diamonds
 
--- An example Hand containing 2 cards
-hand2 :: Hand
-hand2 = [Card (Numeric 2) Hearts, Card Jack Spades]
+aCard2 :: Card
+aCard2 = Card (Numeric 6) Spades
 
--- Produces a list containing several identical elements
--- of the type Int, provided (length hand2) == 2
+aCard3 :: Card
+aCard3 = Card Ace Spades
+
+aHand :: Hand 
+aHand = [aCard1, aCard2]
+
+aHand2 = [Card (Numeric 8) Hearts, Card Ace Diamonds]
+
+--Task A1
+--Example showing how a recursive function would process a list
+
 sizeSteps :: [Int]
-sizeSteps = [ size hand2
+sizeSteps = [ size aHand2
             , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
             , 1 + size (Card Jack Spades : [])
             , 1 + (1 + size [])
@@ -26,78 +39,72 @@ sizeSteps = [ size hand2
             , 2
             ]
 
+--Task A2
+--Functions used for displaying cards and hands neatly for the user
 
--- A2 ------------------------
-
-
--- Two predetermined cards used for aHand
-aCard1 :: Card
-aCard1 = Card Ace Spades
-
-aCard2 :: Card
-aCard2 = Card (Numeric 7) Clubs
-
--- A hand containing two predetermined cards
-aHand :: Hand
-aHand = [aCard1, aCard2]
-
-
--- Displays a given Card as a string reading
--- which card it is in common tongue
+--Displays a card in the format "x of y"
 displayCard :: Card -> String
-displayCard (Card (Numeric r) s)    = (show r) ++ " of " 
-                                   ++ (show s)
-displayCard (Card r s)              = "The " ++ (show r) 
-                                   ++ " of " ++ (show s)
+displayCard (Card (Numeric r) s) = show (r) ++ " of " ++ show(s) 
+displayCard (Card r s)           = show (r) ++ " of " ++ show(s) 
 
-
--- Displays a given hand as a string telling you
--- how many, and which cards are in the hand
+--Displays all cards in a hand using previous function and list comprehension
 display :: Hand -> String
-display hand            =  "You have " ++ (show(size hand))
-                        ++ " cards on hand:\n" 
-                        ++ unlines [displayCard card | card <- hand]
+display hand = "Your hand: \n" ++ unlines [ " >  " ++displayCard card | card <- hand]
 
+--Example: putStr is necessary to aquire the right newline formatting
+displayExamp = putStr (display aHand2)  
 
--- A3 ------------------------
+--Task A3
+--Functions counting the points given to the player for having cards/hands
 
--- Assigns all card ranks a corresponding numeric value
+--Returns points associated with certain ranks
 valueRank :: Rank -> Int
-valueRank (Numeric n)    = n
-valueRank Ace            = 11
-valueRank n              = 10
+valueRank (Numeric r)   = r
+--Aces are worth one if sum of hand exceeds 21
+valueRank Ace           = 11 
+valueRank r             = 10
 
--- Counts how many aces are in a given hand
+--Counts the number of aces in a hand
 numberOfAces :: Hand -> Int
-numberOfAces []                   = 0
-numberOfAces ((Card Ace _):hand)  = 1 + (numberOfAces hand)
-numberOfAces (card:hand)          = numberOfAces hand
+numberOfAces [] = 0
+numberOfAces (card:hand) | rank card == Ace = 1 + numberOfAces(hand)
+                         | otherwise = numberOfAces(hand)
 
+--Counts the total value of a players hand
+--Deletes 10 points for each ace if value > 21
 
+value :: Hand -> Int
+value [] = 0
+value (card:hand) | valueRank(rank(card)) + value(hand) > 21 
+                   && numberOfAces(card:hand) > 0
+                   = valueRank(rank(card)) + value(hand) 
+                   - (10 * numberOfAces(card:hand)) 
+                  | otherwise 
+                   = valueRank(rank(card)) + value(hand) 
+
+-- Calculates the total value of a given hand
+
+{-
 -- Fetches the value of a card depending on its rank
 valueCard :: Card -> Int
 valueCard (Card r _)     = valueRank r
 
--- Calculates the total value of a given hand
 value :: Hand -> Int
 value []   = 0
-value hand | ((sum [valueCard card | card <- hand]) > 21)
+value hand | ((sum [valueRank(rank(card)) card | card <- hand]) > 21)
              && ((numberOfAces hand) > 0) 
-             = (sum [valueCard card | card <- hand]) - 10*(numberOfAces hand)
-           | otherwise = sum [valueCard card | card <- hand]
+             = (sum [valueRank(rank(card)) card | card <- hand]) - 10*(numberOfAces hand)
+           | otherwise = sum [valueRank(rank(card)) card | card <- hand]    -}               
+ 
+    
+--Task A4
+--Functions used for checking various win/lose conditions
 
-
-
--- A4 ------------------------
-
-
--- Determines if the value of a hand exceeds 21
+--Checks if a player is bust
 gameOver :: Hand -> Bool
-gameOver hand = (value hand) > 21
+gameOver hand = value(hand) > 21 
 
-
--- Determines the winner of the round depending on the value of the hands,
--- with both hands being equal in value results in the bank winning
+--Checks which player has won depending on boths hands
 winner :: Hand -> Hand -> Player 
 winner handG handB | not (gameOver(handG)) &&  
                      (value(handG) > value(handB)) = Guest
@@ -105,13 +112,13 @@ winner handG handB | not (gameOver(handG)) &&
                      && (gameOver(handB))          = Guest
                    | otherwise                     = Bank
 
--- B1 ------------------------
+--B1
 
--- A list of all ranks
+--A list of all ranks
 allRanks :: [Rank]
 allRanks  = [Numeric n | n <- [2..10]] ++ [Jack,Queen,King,Ace]
 
--- A list of all suits
+--A list of all suits
 allSuits :: [Suit]
 allSuits = [Hearts, Spades, Diamonds, Clubs]
 
@@ -121,22 +128,21 @@ fullDeck = [Card rank suit | rank <- allRanks , suit <- allSuits]
 
 prop_size_fullDeck = size fullDeck == 52
 
--- B2 ------------------------
+--B2
 
 -- Draws a card from the deck and adds to the hand of whomever drew
 draw :: Deck -> Hand -> (Deck, Hand)
 draw deck hand | deck == [] = error "draw: The deck is empty."
                | otherwise  = ((drop 1 deck), (hand ++ take 1 deck))
 
--- B3 ------------------------
+--B3
 
-
--- Plays for the bank
+--Plays for the bank
 playBank :: Deck -> Hand
 playBank deck = playBank' (deck, [])
 
--- Does the work for playbank
--- Draws cards until the value of bankhand exceeds 16
+--Does the work for playbank
+--Draws cards until the value of bankhand exceeds 16
 playBank' :: (Deck, Hand) -> Hand
 playBank' deckBankH | value(snd(drawHand)) < 17 = playBank'(drawHand)
                     | otherwise = h
@@ -144,16 +150,16 @@ playBank' deckBankH | value(snd(drawHand)) < 17 = playBank'(drawHand)
           h = snd(deckBankH)
           drawHand = draw d h
 
--- B4 ------------------------ 
+--B4 
 
--- Deletes the nth element in a deck
+--Deletes the nth element in a deck
 deleteN :: Int -> Deck -> Deck
 deleteN i deck = xa ++ tail xb
     where 
     xa = fst(splitAt (i) deck) --Beginning of deck 
     xb = snd(splitAt (i) deck) --element i + Rest of deck 
 
--- Shuffles a deck
+--Shuffles a deck
 shuffle :: [Double] -> Deck -> Deck
 shuffle randList deck | deck == []     = deck
                       | otherwise      = (card) : 
@@ -162,25 +168,25 @@ shuffle randList deck | deck == []     = deck
     i = ceiling ((size deck) * (head randList)) - 1 -- Chosen index
     card = deck !! i --The randomly chosen card
 
--- B5 ------------------------ 
--- Properties that the func shuffle must follow
+--Task B5 
+--Properties that the func shuffle must follow
 
--- Helper function for prop_shuffle
+--Helper function for prop_shuffle
 belongsTo :: Card -> Deck -> Bool
 c `belongsTo` []      = False
 c `belongsTo` (c':cs) = c == c' || c `belongsTo` cs
 
--- Checks if there is exactly one of each card in the shuffled deck
+--Checks if there is exactly one of each card in the shuffled deck
 prop_shuffle :: Card -> Deck -> Rand -> Bool
 prop_shuffle card deck (Rand randomlist) =
     card `belongsTo` deck == card `belongsTo` shuffle randomlist deck
 
--- Checks if the length of the deck is the same as before shuffling
+--Checks if the length of the deck is the same as before shuffling
 prop_size_shuffle :: Rand -> Deck -> Bool
 prop_size_shuffle (Rand randomlist) deck = 
                    length(deck) == length(shuffle randomlist deck)
 
--- B6 ------------------------
+--B6
 
 implementation = Interface
   {  iFullDeck  = fullDeck
@@ -195,3 +201,4 @@ implementation = Interface
 
 main :: IO ()
 main = runGame implementation
+
